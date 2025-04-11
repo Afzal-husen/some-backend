beb3abb37a5eab996bf9e28a216edcbf5502302e
Afzal Diwan <afzalhusen3@gmail.com>
2025-04-11 13:02:34 +0530

    dockerized

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 const app = express();
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
 });
 app.get("/", (req, res) => {
     res.send("Hello World");
